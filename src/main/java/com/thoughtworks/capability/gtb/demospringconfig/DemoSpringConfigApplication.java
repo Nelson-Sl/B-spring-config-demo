98e4c1afd020447c9d45f2c67b76f18f7f0dd193
Xiaofeng Wang <xfun68@gmail.com>
2020-05-12 21:45:31 +0800

    adds code for demo-1-2

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplication.java
@@ -1,18 +1,26 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DemoSpringConfigApplication implements ApplicationRunner {
 
-//	@Value("foobar")
-	@Value("${foobar}")
-//	private String foobar;
-	private int foobar;
+	@Autowired
+	private final Environment env;
+
+	@Autowired
+	private final ConfigurableEnvironment configurableEnv;
+
+	public DemoSpringConfigApplication(Environment env, ConfigurableEnvironment configurableEnv) {
+		this.env = env;
+		this.configurableEnv = configurableEnv;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringConfigApplication.class, args);
@@ -20,6 +28,7 @@ public class DemoSpringConfigApplication implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		System.out.println("foobar is " + foobar);
+		configurableEnv.getSystemProperties().forEach((key, value) -> System.out.println(key + ": " + value));
+		System.out.println("foobar is " + env.getProperty("foobar"));
 	}
 }
