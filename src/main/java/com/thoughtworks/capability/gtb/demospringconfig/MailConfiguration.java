afec218cf466cb6df13769cded49784d5b8eb7f4
Xiaofeng Wang <xfun68@gmail.com>
2020-05-12 22:34:59 +0800

    adds code for demo-5

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfiguration.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfiguration.java
@@ -1,11 +1,20 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Max;
+
+@Validated
 @ConfigurationProperties("mail")
 public class MailConfiguration {
 
+    @Length(min = 16, max = 64)
     private String hostname;
+    @Min(9001)
+    @Max(9999)
     private int port;
     private String from;
 
