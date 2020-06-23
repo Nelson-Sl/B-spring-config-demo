1cc9610da1411dec7565fff7ac9f729fbb15cec9
Xiaofeng Wang <xfun68@gmail.com>
2020-06-23 18:40:10 +0800

    add output statments

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplication.java
@@ -1,13 +1,23 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoSpringConfigApplication {
+public class DemoSpringConfigApplication implements ApplicationRunner {
+
+	@Value("${server.port}")
+	private int port;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringConfigApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("port is " + port);
+	}
 }
